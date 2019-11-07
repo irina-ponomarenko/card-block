79aaa5a9746deef0a23292fc9759341efb9fb282
Admin <ponomarenko02041993@gmail.com>
2019-11-07 12:14:47 +0200

    new

--- js/main.js
+++ js/main.js
@@ -20,4 +20,31 @@ $(document).ready(function() {
         sync: "#carousel"
     });
 
+    //----------check item color----------------//
+
+    $('.color-item').on('click', function () {
+       $(".color-item").removeClass('active-item-color');
+       $(this).addClass('active-item-color');
+       $('.other-color').removeClass('active-item-color');
+
+    });
+
+
+    //------------counter------------------------//
+
+    $('.minus').on('click', function () {
+        let $input = $(this).parent().find('.input-counter');
+        let count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').on('click',function () {
+        let $input = $(this).parent().find('.input-counter');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+
 });
